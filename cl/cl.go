eb51b50257fca4643905e7184c5acc44f3242348
mswift42 <martin.haesler@gmail.com>
2019-05-15 15:05:14 +0100

    insert app help template.

--- cl/cl.go
+++ cl/cl.go
@@ -12,5 +12,23 @@ func InitCli() *cli.App {
 	app.Version = "0.0.1"
 	app.Copyright = "2019 (c) Martin Haesler"
 	app.Usage = "generate custom ui plugins for IntelliJ editors > 191"
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+	
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command 
+	[command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments. ..]{{end}}
+AUTHOR: {{.Author}}
+   {{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+COPYRIGHT:
+   {{.Copyright}}
+VERSION:
+   {{.Version}}
+`
 	return app
 }
