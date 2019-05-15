10585b02d68dce8891861271bb9332f3db0d1a5b
mswift42 <martin.haesler@gmail.com>
2019-05-15 13:22:45 +0100

    set author/version/copyright.

--- cl/cl.go
+++ cl/cl.go
@@ -6,5 +6,11 @@ import (
 
 func InitCli() *cli.App {
 	app := cli.NewApp()
+	app.Setup()
+	app.Name = "iui"
+	app.Author = "Martin Haesler"
+	app.Version = "0.0.1"
+	app.Copyright = "2019 (c) Martin Haesler"
+	app.Usage = "generate custom ui plugins for IntelliJ editors > 191"
 	return app
 }
